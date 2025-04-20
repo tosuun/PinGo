f27b2e014da82bde33e0bdd3693c72f99f88a996
tosunn <143181573+tosuun@users.noreply.github.com>
2025-04-20 21:24:04 +0300

    Update users.java

--- model/users.java
+++ model/users.java
@@ -1,1 +1,47 @@
+public class users {
+	private int idUser;
+	private String userName;
+	private String userMail;
+	private String password;
+	private String userPhone;
+	private String userType;
+	public int getIdUser() {
+		return idUser;
+	}
+	public void setIdUser(int idUser) {
+		this.idUser = idUser;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserMail() {
+		return userMail;
+	}
+	public void setUserMail(String userMail) {
+		this.userMail = userMail;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getUserPhone() {
+		return userPhone;
+	}
+	public void setUserPhone(String userPhone) {
+		this.userPhone = userPhone;
+	}
+	public String getUserType() {
+		return userType;
+	}
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+	
+	
+}
 
