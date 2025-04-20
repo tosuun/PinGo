b5b58cca5af35b9666db5b71814a1312a0bc2a0c
tosunn <143181573+tosuun@users.noreply.github.com>
2025-04-20 21:18:41 +0300

    Update sales.java

--- model/sales.java
+++ model/sales.java
@@ -1,1 +1,33 @@
 
+public class sales {
+	private int idSales;
+	private String sellerName;
+	private String customerName;
+	private String product;
+	public int getIdSales() {
+		return idSales;
+	}
+	public void setIdSales(int idSales) {
+		this.idSales = idSales;
+	}
+	public String getSellerName() {
+		return sellerName;
+	}
+	public void setSellerName(String sellerName) {
+		this.sellerName = sellerName;
+	}
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	public String getProduct() {
+		return product;
+	}
+	public void setProduct(String product) {
+		this.product = product;
+	}
+
+}
+
