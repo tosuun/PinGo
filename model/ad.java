17b0d2cd200ae17c2b0c195aaf2ebba68348ad4c
tosunn <143181573+tosuun@users.noreply.github.com>
2025-04-20 21:22:18 +0300

    Update ad.java

--- model/ad.java
+++ model/ad.java
@@ -1,1 +1,61 @@
+public class ad {
+	private int idAd;
+	private String product;
+	private String sellerName;
+	private int price;
+	private int stock;
+	private boolean isActive;
+	private String createdAt;
+	private String imageUrl;
+	public int getIdAd() {
+		return idAd;
+	}
+	public void setIdAd(int idAd) {
+		this.idAd = idAd;
+	}
+	public String getProduct() {
+		return product;
+	}
+	public void setProduct(String product) {
+		this.product = product;
+	}
+	public String getSellerName() {
+		return sellerName;
+	}
+	public void setSellerName(String sellerName) {
+		this.sellerName = sellerName;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getStock() {
+		return stock;
+	}
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	public boolean isActive() {
+		return isActive;
+	}
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+	public String getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(String createdAt) {
+		this.createdAt = createdAt;
+	}
+	public String getImageUrl() {
+		return imageUrl;
+	}
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+	
+	
+}
 
