ec2f791dd08ecd93d30dc84fa00564bed1910898
tosunn <143181573+tosuun@users.noreply.github.com>
2025-04-20 15:31:10 +0300

    Update DbHelper.java

--- util/DbHelper.java
+++ util/DbHelper.java
@@ -1,1 +1,27 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class DbHelper {
+	// Veritabanı bağlantı bilgileri
+	private static final String  DB_USER = "root";
+	private static final String DB_PASSWORD = "12345";
+	private static final String DB_URL = "jdbc:mysql://localhost:3307/webdb";
+	 /**
+     * Veritabanına yeni bir bağlantı döndürür.
+     * @return Connection nesnesi
+     * @throws SQLException Bağlantı hatası olursa
+     */
+	public Connection getConnection() throws SQLException{
+		return DriverManager.getConnection(DB_URL,DB_USER,DB_PASSWORD);
+	}
+	public static void closeConnection(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace(); // Hata loglama
+            }
+        }
+	}
+}
