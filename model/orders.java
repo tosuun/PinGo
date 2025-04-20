b00dc7ca436128870358de295ee12f840b0b2bae
tosunn <143181573+tosuun@users.noreply.github.com>
2025-04-20 21:19:02 +0300

    Update orders.java

--- model/orders.java
+++ model/orders.java
@@ -1,1 +1,46 @@
-
+public class orders {
+	private int idOrder;
+	private String orderDate;
+	private String customer;
+	private int totalAmount;
+	private String status;
+	private String order_item;
+	public int getIdOrder() {
+		return idOrder;
+	}
+	public void setIdOrder(int idOrder) {
+		this.idOrder = idOrder;
+	}
+	public String getOrderDate() {
+		return orderDate;
+	}
+	public void setOrderDate(String orderDate) {
+		this.orderDate = orderDate;
+	}
+	public String getCustomer() {
+		return customer;
+	}
+	public void setCustomer(String customer) {
+		this.customer = customer;
+	}
+	public int getTotalAmount() {
+		return totalAmount;
+	}
+	public void setTotalAmount(int totalAmount) {
+		this.totalAmount = totalAmount;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getOrder_item() {
+		return order_item;
+	}
+	public void setOrder_item(String order_item) {
+		this.order_item = order_item;
+	}
+	
+	
+}
