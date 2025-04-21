4edaaddc95fcbfa93421617ca9c925ad2b4ec18d
YorgunSavasci <yasinunal157@gmail.com>
2025-04-21 23:55:51 +0300

    users.java

--- model/users.java
+++ model/users.java
@@ -1,47 +1,96 @@
+package model;
+
 public class users {
-	private int idUser;
-	private String userName;
-	private String userMail;
-	private String password;
-	private String userPhone;
-	private String userType;
-	public int getIdUser() {
-		return idUser;
-	}
-	public void setIdUser(int idUser) {
-		this.idUser = idUser;
-	}
-	public String getUserName() {
-		return userName;
-	}
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-	public String getUserMail() {
-		return userMail;
-	}
-	public void setUserMail(String userMail) {
-		this.userMail = userMail;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getUserPhone() {
-		return userPhone;
-	}
-	public void setUserPhone(String userPhone) {
-		this.userPhone = userPhone;
-	}
-	public String getUserType() {
-		return userType;
-	}
-	public void setUserType(String userType) {
-		this.userType = userType;
-	}
-	
-	
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+    private String role;         // "seller" ya da "buyer"
+    private String telNo;
+    
+    // Satıcıya özel alanlar
+    private int satisSayisi;
+    private String tc;
+    private double ciro;
+
+    public users() {}
+
+    // Constructor
+    public users(int id, String name, String email, String password, String role,
+                 String telNo, int satisSayisi, String tc, double ciro) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.telNo = telNo;
+        this.satisSayisi = satisSayisi;
+        this.tc = tc;
+        this.ciro = ciro;
+    }
+
+    // Getter ve Setter'lar
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getTelNo() {
+        return telNo;
+    }
+    public void setTelNo(String telNo) {
+        this.telNo = telNo;
+    }
+
+    public int getSatisSayisi() {
+        return satisSayisi;
+    }
+    public void setSatisSayisi(int satisSayisi) {
+        this.satisSayisi = satisSayisi;
+    }
+
+    public String getTc() {
+        return tc;
+    }
+    public void setTc(String tc) {
+        this.tc = tc;
+    }
+
+    public double getCiro() {
+        return ciro;
+    }
+    public void setCiro(double ciro) {
+        this.ciro = ciro;
+    }
 }
 
